29401455909368c1a5397a17fd606e124d3917c8
oliver-sys <jamesoliverg@outlook.com>
2023-09-29 13:37:22 +0800

    Added all types of variables

--- Midterms/Variable.php
+++ Midterms/Variable.php
@@ -8,9 +8,34 @@
 <body>
     
     <?php
-    $nice = 69;
-    echo $nice;
+    $intvar =  420;
+    echo "This is integer: ";
+    echo $intvar;
     
+
+    echo "<br>";
+    define("message", "This is constant: ");
+    define ("nice", 69);
+    echo message;
+    echo nice; 
+
+    echo "<br>";
+    echo "This is integer with operations: ";
+    $a =  $intvar + 1;
+    echo $a;
+
+    echo "<br>";
+    $dvar =  10.1;
+    echo "This is double: ";
+    echo $dvar;
+
+    echo "<br>";
+    $isNice = true;
+    if ($isNice) {
+        echo "This is nice!(This is for the usage of the boolean variable.)\n";
+    } else {
+        echo "Not nice.\n";
+    }
     ?>
 
 </body>
